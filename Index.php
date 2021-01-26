f78b9aded6061ee2969547ec929b7c25121390f0
tuanfront_end <atuanvptd@gmail.com>
2021-01-26 16:18:35 +0700

    change file common

--- Index.php
+++ Index.php
@@ -1,5 +1,5 @@
 <?php get_header() ?>
-<div class="container">
+<div class="">
     <h1>HS-BLOG 2</h1>
 </div>
 <?php get_footer() ?>
--- single.php
+++ single.php
@@ -1,38 +1,33 @@
 <?php get_header(); ?>
-<div class="breadcrumbs">
-    <?php if(function_exists('bcn_display'))
-    {
-        bcn_display();
-    }?>
-</div>
-<div class="container">
-    <div class="main">
-        <?php if (have_posts()): while (have_posts()):the_post();?>
-            <article class="post">
-                <h3>
-                    <a href="<?php the_permalink();?>"><?php the_title();?></a>
-                </h3>
-                <div class="meta">
-                    Created By
-                    <a href="<?php echo get_author_posts_url(get_the_author_meta('ID'));?>">
-                        <?php the_author();?>
-                    </a>
-                    <?php the_time('F j,Y g:i a');?>
-                </div>
-                <?php if(has_post_thumbnail()):?>
-                    <div class="post-thumbnail">
-                        <?php the_post_thumbnail();?>
+
+<div class="">
+    <div class="">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <article class="post">
+                    <h3>
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </h3>
+                    <div class="meta">
+                        Created By
+                        <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
+                            <?php the_author(); ?>
+                        </a>
+                        <?php the_time('F j,Y g:i a'); ?>
                     </div>
-                <?php endif;?>
-                <?php the_content();?>
-            </article>
-        <?php endwhile;?>
-        <?php endif;?>
-        <?php comments_template();?>
+                    <?php if (has_post_thumbnail()) : ?>
+                        <div class="post-thumbnail">
+                            <?php the_post_thumbnail(); ?>
+                        </div>
+                    <?php endif; ?>
+                    <?php the_content(); ?>
+                </article>
+            <?php endwhile; ?>
+        <?php endif; ?>
+        <?php comments_template(); ?>
     </div>
 
     <?php
-    if (is_active_sidebar('sidebar-right')){
+    if (is_active_sidebar('sidebar-right')) {
         get_sidebar();
     } ?>
 
@@ -40,9 +35,10 @@
 </div>
 <footer>
     <div class="container">
-        <p>&copy; 2020 - <?php bloginfo('title');?></p>
+        <p>&copy; 2020 - <?php bloginfo('title'); ?></p>
     </div>
 </footer>
-<?php wp_footer();?>
+<?php wp_footer(); ?>
 </body>
+
 </html>

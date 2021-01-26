b6a2a821e4d2a56c692d16ce192f8875514fc09c
tuanfront_end <atuanvptd@gmail.com>
2021-01-26 12:09:00 +0700

    addd css

--- functions.php
+++ functions.php
@@ -2,24 +2,56 @@
 
 function hs2_register_styles()
 {
-	wp_enqueue_style('hs2-line-awesome-style',
-		get_template_directory_uri() . '/assets/fonts/line-awesome-1.3.0/css/line-awesome.min.css', [], '1.0', 'all');
-	wp_enqueue_style('hs2-glide-core-style',
-		get_template_directory_uri() . '/assets/js/glide-3.4.1/dist/css/glide.core.min.css', [], '1.0', 'all');
-	wp_enqueue_style('hs2-glide-theme-style',
-		get_template_directory_uri() . '/assets/js/glide-3.4.1/dist/css/glide.theme.min.css', [], '1.0', 'all');
-	wp_enqueue_style('hs2-sub-style',
-		get_template_directory_uri() . '/assets/css/styles.css', [], '1.0', 'all');
+	wp_enqueue_style(
+		'hs2-line-awesome-style',
+		get_template_directory_uri() . '/assets/fonts/line-awesome-1.3.0/css/line-awesome.min.css',
+		[],
+		'1.0',
+		'all'
+	);
+	wp_enqueue_style(
+		'hs2-glide-core-style',
+		get_template_directory_uri() . '/assets/js/glide-3.4.1/dist/css/glide.core.min.css',
+		[],
+		'1.0',
+		'all'
+	);
+	wp_enqueue_style(
+		'hs2-glide-theme-style',
+		get_template_directory_uri() . '/assets/js/glide-3.4.1/dist/css/glide.theme.min.css',
+		[],
+		'1.0',
+		'all'
+	);
+	wp_enqueue_style(
+		'hs2-sub-style',
+		get_template_directory_uri() . '/assets/css/styles.css',
+		[],
+		'1.0',
+		'all'
+	);
 	wp_enqueue_style('hs2-main-style', get_template_directory_uri() . '/style.css', [], '1.0', 'all');
 }
 
-add_action('wp_enqueue_scripts', 'hs2_register_styles');
+if (defined('ELEMENTOR_VERSION')) {
+	add_action('elementor/frontend/after_enqueue_styles', 'hs2_register_styles');
+} else {
+	add_action('wp_enqueue_scripts', 'hs2_register_styles');
+}
+
 
 function hs2_register_scripts()
 {
-	wp_enqueue_script('glide-js', get_template_directory_uri() . '/assets/js/glide-3.4.1/dist/glide.min.js', [], '1.0',
-		false);
+	wp_enqueue_script(
+		'glide-js',
+		get_template_directory_uri() . '/assets/js/glide-3.4.1/dist/glide.min.js',
+		[],
+		'1.0',
+		false
+	);
 	wp_enqueue_script('hs2-main-script', get_template_directory_uri() . '/assets/js/script.js', [], '1.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'hs2_register_scripts');
+// 
+add_theme_support('post-thumbnails');
